18c686580a6cdcffd95265b84f3e00f31ad10560
JustTRS <justtrs@gmail.com>
2022-11-22 01:01:34 +0100

    Create graph from text option

--- src/components/SimplePanel.tsx
+++ src/components/SimplePanel.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import { PanelProps } from '@grafana/data';
 import { SimpleOptions } from 'types';
 import { css, cx } from '@emotion/css';
-import { useStyles2, useTheme2 } from '@grafana/ui';
+import { useStyles2 } from '@grafana/ui';
+
+import { Mermaid, MermaidProps } from "./Mermaid";
 
 interface Props extends PanelProps<SimpleOptions> {}
 
@@ -12,23 +14,15 @@ const getStyles = () => {
       font-family: Open Sans;
       position: relative;
     `,
-    svg: css`
-      position: absolute;
-      top: 0;
-      left: 0;
-    `,
-    textBox: css`
-      position: absolute;
-      bottom: 0;
-      left: 0;
-      padding: 10px;
-    `,
   };
 };
 
 export const SimplePanel: React.FC<Props> = ({ options, data, width, height }) => {
-  const theme = useTheme2();
   const styles = useStyles2(getStyles);
+  console.log(styles);
+  const mermaidProps: MermaidProps = {
+    text: options.text
+  };
   return (
     <div
       className={cx(
@@ -39,23 +33,7 @@ export const SimplePanel: React.FC<Props> = ({ options, data, width, height }) =
         `
       )}
     >
-      <svg
-        className={styles.svg}
-        width={width}
-        height={height}
-        xmlns="http://www.w3.org/2000/svg"
-        xmlnsXlink="http://www.w3.org/1999/xlink"
-        viewBox={`-${width / 2} -${height / 2} ${width} ${height}`}
-      >
-        <g>
-          <circle style={{ fill: theme.colors.primary.main }} r={100} />
-        </g>
-      </svg>
-
-      <div className={styles.textBox}>
-        {options.showSeriesCount && <div>Number of series: {data.series.length}</div>}
-        <div>Text option value: {options.text}</div>
-      </div>
+      <Mermaid {...mermaidProps}/>
     </div>
   );
 };
--- src/module.ts
+++ src/module.ts
@@ -7,34 +7,13 @@ export const plugin = new PanelPlugin<SimpleOptions>(SimplePanel).setPanelOption
     .addTextInput({
       path: 'text',
       name: 'Simple text option',
-      description: 'Description of panel option',
-      defaultValue: 'Default value of text input option',
-    })
-    .addBooleanSwitch({
-      path: 'showSeriesCount',
-      name: 'Show series counter',
-      defaultValue: false,
-    })
-    .addRadio({
-      path: 'seriesCountSize',
-      defaultValue: 'sm',
-      name: 'Series counter size',
+      description: 'Graph definition',
+      defaultValue: `flowchart LR
+A --> B
+B --> C
+`,
       settings: {
-        options: [
-          {
-            value: 'sm',
-            label: 'Small',
-          },
-          {
-            value: 'md',
-            label: 'Medium',
-          },
-          {
-            value: 'lg',
-            label: 'Large',
-          },
-        ],
-      },
-      showIf: (config) => config.showSeriesCount,
+        useTextarea: true
+      }
     });
 });
